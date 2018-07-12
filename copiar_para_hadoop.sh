221b57fec6d1d816bb7c67f9c5ce387ac7264429
Francisco Keppler Silva Alecrim <alecrim@gmail.com>
2018-07-11 23:24:06 -0300

    ajuste para paralelizar cópias

--- copiar_para_hadoop.sh
+++ copiar_para_hadoop.sh
@@ -25,9 +25,37 @@ for ANO in $(seq 2013 2018); do
       CONT_S=$CONT
     fi
     echo "Copiando ${ANO}${CONT_S}_BolsaFamilia_Pagamentos.csv"
-    hdfs dfs -copyFromLocal dados/${ANO}${CONT_S}_BolsaFamilia_Pagamentos.csv /input
+    hdfs dfs -copyFromLocal dados/${ANO}${CONT_S}_BolsaFamilia_Pagamentos.csv /input & 
+    echo $! > dados/pids/$!
     CONT=$(($CONT + 1))
   done
 done
+
+
+
+echo "Aguardando"
+cd dados/pids
+while [ true ]; do
+  echo "Verificando processos ativos..."
+  for x in *; do
+    echo $x
+    if [ "$(ps -aux | grep $x | wc -l)" == "1" ]; then
+      #echo "Removendo $x"
+      rm $x
+    #else
+      #echo "N remover $x"
+    fi
+  done
+  if [ "$(ls)" == "" ]; then
+    break
+  else
+   echo "."
+   sleep 5
+  fi
+done
+
+
+
+
 echo "Copia para o hadoop finalizada"
 date
