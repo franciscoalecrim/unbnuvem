4e73cd11ffbf88406b90db3b917d83afa8ca5b5e
Francisco Keppler Silva Alecrim <alecrim@gmail.com>
2018-07-11 23:51:45 -0300

    copiando por ano

--- copiar_para_hadoop.sh
+++ copiar_para_hadoop.sh
@@ -17,6 +17,7 @@ hdfs dfs -mkdir /input
 ###
 echo "Copiando arquivos para o HDFS"
 for ANO in $(seq 2013 2018); do 
+  echo "Copiando ano de ${ANO}"
   CONT=1
   while [ "$CONT" -le $MESES ]; do
     if [ "$CONT" -le 9 ]; then
@@ -29,30 +30,32 @@ for ANO in $(seq 2013 2018); do
     echo $! > dados/pids/$!
     CONT=$(($CONT + 1))
   done
-done
 
-
-
-echo "Aguardando"
-cd dados/pids
-while [ true ]; do
-  echo "Verificando processos ativos..."
-  for x in *; do
-    echo $x
-    if [ "$(ps -aux | grep $x | wc -l)" == "1" ]; then
-      #echo "Removendo $x"
-      rm $x
-    #else
-      #echo "N remover $x"
+  echo "Aguardando ano de ${ANO}"
+  cd dados/pids
+  while [ true ]; do
+    echo "Verificando processos ativos..."
+    for x in *; do
+      echo $x
+      if [ "$(ps -aux | grep $x | wc -l)" == "1" ]; then
+        #echo "Removendo $x"
+        rm $x
+      #else
+        #echo "N remover $x"
+      fi
+    done
+    if [ "$(ls)" == "" ]; then
+      break
+    else
+    echo "."
+    sleep 5
     fi
   done
-  if [ "$(ls)" == "" ]; then
-    break
-  else
-   echo "."
-   sleep 5
-  fi
+
 done
+
+
+
 
 
 
